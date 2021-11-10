15a47c0b00274ec3a38c7b450cbd5d3594bdad3e
skyn3t-sscp <emamialidev@gmail.com>
2021-11-10 00:40:21 -0800

    fix social buttons

--- src/components/welcomeMessage/WelcomeMessage.js
+++ src/components/welcomeMessage/WelcomeMessage.js
@@ -13,50 +13,53 @@ const WelcomeMessage = props => {
 
   return (
     <>
-    <div className="welcomeContainer" ref={welcomeRef}>
-      <div className="welcomeLeftSide">
-        <div className="textTitleContainer">
-          <p className="welcomeTitle">Welcome to the Super Stoner Costume party at Royalty Ln.</p>
-          <p className="welcomeBody">A group of 10,000 Stoners each received an invite to an exclusive costume party from an undisclosed number. Each stoner is an alcohol drinking, kush smoking, party animal who loves good vibes. </p>
+      <div className="welcomeContainer" ref={welcomeRef}>
+        <div className="welcomeLeftSide">
+          <div className="textTitleContainer">
+            <p className="welcomeTitle">Welcome to the Super Stoner Costume party at Royalty Ln.</p>
+            <p className="welcomeBody">A group of 10,000 Stoners each received an invite to an exclusive costume party from an undisclosed number. Each stoner is an alcohol drinking, kush smoking, party animal who loves good vibes. </p>
+          </div>
+        </div>
+        <div className="welcomeRightSide">
+          <div className="socialContainer">
+            <div style={{ marginBottom: '32px', marginTop: '-45px' }}>
+              <span className="joinSocial">Join our discord</span>
+              <a
+                className="socialIcon"
+                href="https://discord.gg/qK453kMFM3"
+                target="_blank"
+                rel="noreferrer"
+                aria-label="Discord"
+              >
+                <i className="fab fa-discord"></i>
+              </a>
+            </div>
+            <div style={{ display: 'flex', justifyContent: 'space-between', marginBottom: '32px' }}>
+              <span className="joinSocial">Follow us on</span>
+              <a
+                className="socialIcon"
+                href="https://twitter.com/SSCostumeParty"
+                target="_blank"
+                rel="noreferrer"
+                aria-label="Twitter"
+              >
+                <i className="fab fa-twitter"></i>
+              </a>
+            </div>
+            <div style={{ display: 'flex', justifyContent: 'flex-end' }}>
+              <Link
+                className="socialIcon"
+                to="https://www.instagram.com/superstonercostumeparty/"
+                target="_blank"
+                aria-label="Instagram"
+              >
+                <i className="fab fa-instagram"></i>
+              </Link>
+            </div>
+          </div>
         </div>
       </div>
-      <div className="welcomeRightSide">
-        <div className="socialContainer">
-        <div>
-          <span className="joinSocial">Join our discord</span>
-          <a
-              className="socialIcon"
-              href="https://discord.gg/qK453kMFM3"
-              target="_blank"
-              rel="noreferrer"
-              aria-label="Discord"
-            >
-              <i className="fab fa-discord"></i>
-            </a>
-        </div>
-          <span className="joinSocial">Follow us on</span>
-          <a
-            style={{ marginRight: '58px'}}
-            className="socialIcon"
-            href="https://twitter.com/SSCostumeParty"
-            target="_blank"
-            rel="noreferrer"
-            aria-label="Twitter"
-          >
-            <i className="fab fa-twitter"></i>
-          </a>
-          <Link
-              className="socialIcon"
-              to="https://www.instagram.com/superstonercostumeparty/"
-              target="_blank"
-              aria-label="Instagram"
-            >
-              <i className="fab fa-instagram"></i>
-            </Link>
-        </div>
-      </div>
-    </div>
-    <hr className="welcomeContainer" style={{ marginBottom: '80px' }} />
+      <hr className="welcomeContainer" style={{ marginBottom: '80px' }} />
     </>
   )
 }
