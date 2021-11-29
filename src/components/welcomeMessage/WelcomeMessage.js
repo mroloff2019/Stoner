0e1f2c89d9e9a2280f53eea5e1ffee583b8f5648
skyn3t-sscp <emamialidev@gmail.com>
2021-11-28 22:11:29 -0800

    styling fixes for v2

--- src/components/welcomeMessage/WelcomeMessage.js
+++ src/components/welcomeMessage/WelcomeMessage.js
@@ -14,50 +14,53 @@ const WelcomeMessage = props => {
   return (
     <>
       <div className="welcomeContainer" ref={welcomeRef}>
-        <div className="welcomeLeftSide">
           <div className="textTitleContainer">
             <p className="welcomeTitle">Welcome to the Super Stoner Costume party at Royalty Ln.</p>
             <p className="welcomeBody">A group of 10,000 Stoners each received an invite to an exclusive costume party from an undisclosed number. Each stoner is an alcohol drinking, kush smoking, party animal who loves good vibes. </p>
           </div>
-        </div>
-        <div className="welcomeRightSide">
           <div className="socialContainer">
-            <div style={{ marginBottom: '32px', marginTop: '-45px' }}>
-              <span className="joinSocial">Join our discord</span>
-              <a
-                className="socialIcon"
-                href="https://discord.gg/qK453kMFM3"
-                target="_blank"
-                rel="noreferrer"
-                aria-label="Discord"
-              >
-                <i className="fab fa-discord"></i>
-              </a>
+            <div style={{ display: 'flex', justifyContent: 'center', marginTop: '90px'}}>
+              <p className="socialTitle">CONNECT WITH US TO SEE WHO’S WEARING WHAT</p>
             </div>
-            <div style={{ display: 'flex', justifyContent: 'space-between', marginBottom: '32px' }}>
-              <span className="joinSocial">Follow us on</span>
-              <a
-                className="socialIcon"
-                href="https://twitter.com/SSCostumeParty"
-                target="_blank"
-                rel="noreferrer"
-                aria-label="Twitter"
-              >
-                <i className="fab fa-twitter"></i>
-              </a>
+            <div>
+            <div style={{ display: 'flex', justifyContent: 'center', paddingTop: '36px', marginBottom: '40px' }}>
+              <div style={{ marginRight: '64px' }}>
+                  <Link
+                    className="socialIcon"
+                    to="https://www.instagram.com/superstonercostumeparty/"
+                    target="_blank"
+                    aria-label="Instagram"
+                  >
+                    <i className="fab fa-instagram"></i>
+                  </Link>
+                </div>
+              <div style={{ marginRight: '64px' }}>
+                <a
+                  className="socialIcon"
+                  href="https://discord.gg/qK453kMFM3"
+                  target="_blank"
+                  rel="noreferrer"
+                  aria-label="Discord"
+                >
+                  <i className="fab fa-discord"></i>
+                </a>
+              </div>
+              <div>
+                <a
+                  className="socialIcon"
+                  href="https://twitter.com/SSCostumeParty"
+                  target="_blank"
+                  rel="noreferrer"
+                  aria-label="Twitter"
+                >
+                  <i className="fab fa-twitter"></i>
+                </a>
+              </div>
             </div>
-            <div style={{ display: 'flex', justifyContent: 'flex-end'}}>
-              <Link
-                className="socialIcon"
-                to="https://www.instagram.com/superstonercostumeparty/"
-                target="_blank"
-                aria-label="Instagram"
-              >
-                <i className="fab fa-instagram"></i>
-              </Link>
             </div>
           </div>
-          <div className="mobileSocial">
+         
+        <div className="mobileSocial">
             <p className="mobileSocialText">GET SOCIAL</p>
             <div className="mobileBtnContainer">
               <div>
@@ -94,9 +97,8 @@ const WelcomeMessage = props => {
               </div>
             </div>
           </div>
-        </div>
+      <hr style={{ maxWidth: '1260px', display: 'flex', justifyItems: 'center', margin: '0 auto', marginBottom: '80px' }} />
       </div>
-      <hr className="welcomeContainer divider" style={{ marginBottom: '80px' }} />
     </>
   )
 }
