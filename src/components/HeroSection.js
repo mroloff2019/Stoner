4cbe8eeaa07fa16042eeb39800a4514b201a9842
skyn3t-sscp <emamialidev@gmail.com>
2021-11-28 23:10:43 -0800

    styling fixes for v2

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -6,7 +6,7 @@ import "./HeroSection.css";
 function HeroSection() {
   return (
     <div className="hero-container">
-      <img src={require("../images/houseparty1.png").default} alt="" />
+      <img src={require("../images/houseparty1.png").default} alt=""  style={{ width: '374px', height: 'auto', marginLeft: '16px' }} />
     </div>
   );
 }
